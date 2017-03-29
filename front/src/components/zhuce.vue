9403145e63e8a55f8e663316d7c92000999843f1
luck <1175888622@qq.com>
2017-03-29 11:12:12 +0800

    aa

--- front/src/components/zhuce.vue
+++ front/src/components/zhuce.vue
@@ -34,20 +34,13 @@
       name: 'zhuce',
       data () {
         return {
-           // categorylist:[],
-           // goodslist:[],
-           // currentIndex:0,
-           // isShow:true
            postslist:[],
            password:'',
            username:''
-
-
         }
       },
         methods:{
             changepage(){
-        // router.push(`/goods/detail/${id}`); //es6 字符串模板
              router.push({name:"loading"})
 
          },
@@ -77,7 +70,6 @@
                         }
                     })
                  }
-              
             }
         }
 }
